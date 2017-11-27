7104d2c1842d2dd57f2a64516fd7281463fb48b4
Nil Portugués Calderó <nilopc@gmail.com>
2017-11-27 20:55:45 +0100

    wip

---
+++ src/main/java/com/nilportugues/useragent/app/MainVerticle.java
@@ -0,0 +1,23 @@
+package com.nilportugues.useragent.app;
+
+import io.vertx.core.AbstractVerticle;
+
+public class MainVerticle extends AbstractVerticle {
+
+    @Override
+    public void start() {
+        // your code goes here...
+
+        vertx.createHttpServer().requestHandler(req -> {
+            req.response()
+                .putHeader("content-type", "text/plain")
+                .end("Hello from Vert.x!");
+        }).listen(8080, res -> {
+            if (res.failed()) {
+                res.cause().printStackTrace();
+            } else {
+                System.out.println("Server listening at: http://localhost:8080/");
+            }
+        });
+    }
+}
---
+++ src/main/java/com/nilportugues/useragent/app/parser/Constants.java
@@ -0,0 +1,8 @@
+package com.nilportugues.useragent.app.parser;
+
+/**
+ * @author sojern
+ */
+public class Constants {
+    public static final String EMPTY_STRING = "\"\"";
+}
